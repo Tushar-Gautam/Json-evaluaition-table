47ac33d1ac0ff4480ada5966eca5326813bdf846
Tushar Gautam <tushargautam999@gmail.com>
2024-10-16 13:12:55 +0530

    floating initial working

--- src/components/Filter/index.tsx
+++ src/components/Filter/index.tsx
@@ -1,24 +1,60 @@
 import "./style.css";
 import { EVALUATION_STATUSES, type EvaluationStatus } from "@utils";
-import { FC, useEffect, useRef, useState } from "react";
+import { FC, useEffect, useMemo, useRef, useState } from "react";
 import { FileCheck, FilterIcon } from "lucide-react";
 import { Dropdown } from "../Dropdown";
+import {
+  useFloating,
+  offset,
+  flip,
+  shift,
+  autoUpdate,
+} from "@floating-ui/react";
 
 interface FilterProps {
   selectedStatuses: string[];
   toggleStatus(status: string): void;
 }
+const baseOffset = {
+  crossAxis: 40,
+  alignmentAxis: 10,
+};
 
 export const Filter: FC<FilterProps> = ({ selectedStatuses, toggleStatus }) => {
   const [isFilterOpen, setIsFilterOpen] = useState(false);
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-  const dropdownRef = useRef<HTMLDivElement>(null);
+  const filterRef = useRef<HTMLDivElement>(null);
+  const currentFilterRef = useRef<HTMLDivElement>(null);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  const offsetValue = useMemo(() => {
+    if (windowWidth > 1400) return { ...baseOffset, mainAxis: 250 };
+    if (windowWidth < 768)
+      return {
+        ...baseOffset,
+        crossAxis: 10,
+        mainAxis: -220,
+      };
+    return { ...baseOffset, mainAxis: 250 };
+  }, [windowWidth]);
+
+  const { x, y, strategy, refs, update } = useFloating({
+    placement: "right-start",
+    middleware: [
+      offset(offsetValue),
+      flip({
+        fallbackPlacements: ["bottom-start", "top-start"],
+      }),
+      shift({ padding: 5 }),
+    ],
+    whileElementsMounted: autoUpdate,
+  });
 
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
       if (
-        dropdownRef?.current &&
-        !dropdownRef?.current?.contains(event.target as Node)
+        filterRef?.current &&
+        !filterRef?.current?.contains(event.target as Node)
       ) {
         setIsFilterOpen(false);
         setIsDropdownOpen(false);
@@ -30,6 +66,27 @@ export const Filter: FC<FilterProps> = ({ selectedStatuses, toggleStatus }) => {
       document.removeEventListener("mousedown", handleClickOutside);
     };
   }, []);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  useEffect(() => {
+    if (currentFilterRef.current) {
+      refs.setReference(currentFilterRef.current);
+    }
+  }, [refs, isFilterOpen]);
+
+  useEffect(() => {
+    if (isDropdownOpen) {
+      update();
+    }
+  }, [isDropdownOpen, update]);
 
   const handleToggleAll = () => {
     if (selectedStatuses.length === EVALUATION_STATUSES.length) {
@@ -48,29 +105,40 @@ export const Filter: FC<FilterProps> = ({ selectedStatuses, toggleStatus }) => {
   };
 
   return (
-    <div className="filter-container" ref={dropdownRef}>
+    <div className="filter-container" ref={filterRef}>
       <div onClick={() => setIsFilterOpen(!isFilterOpen)}>
         <FilterIcon color="#4E6187" />
       </div>
-      {isFilterOpen ? (
+      {isFilterOpen && (
         <div className="filters">
           <span className="filter-heading">Evaluation Status</span>
           <div
             className="current-filter"
             onClick={() => setIsDropdownOpen(true)}
+            ref={currentFilterRef}
           >
             <FileCheck height={15} width={15} />
             <span>Current Evaluation Status</span>
           </div>
         </div>
-      ) : null}
-      {isDropdownOpen ? (
-        <Dropdown
-          handleToggleStatus={handleToggleStatus}
-          handleToggleAll={handleToggleAll}
-          selectedStatuses={selectedStatuses}
-        />
-      ) : null}
+      )}
+      {isDropdownOpen && (
+        <div
+          ref={refs.setFloating}
+          style={{
+            position: strategy,
+            top: y ?? 0,
+            left: x ?? 0,
+            width: "max-content",
+          }}
+        >
+          <Dropdown
+            handleToggleStatus={handleToggleStatus}
+            handleToggleAll={handleToggleAll}
+            selectedStatuses={selectedStatuses}
+          />
+        </div>
+      )}
     </div>
   );
 };
